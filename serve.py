cafe718de766d1aaa46327465dca1c8f1a669b5f
SirawitN <nine_sirawit@hotmail.com>
2024-03-29 14:19:59 +0700

    add `predict_fn` (tmp) in serve.py

--- serve.py
+++ serve.py
@@ -25,44 +25,46 @@ def model_fn(model_dir):
     logger.info('Done loading model.')
     return model
 
+def predict_fn(input_data, model):
+    logger.info('Generating cartoonization.')
+	
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+	
+    ext = os.path.splitext(input_data)[1]
+    if ext not in ['.jpg', '.png']:
+        logger.info('Invalid input type.')
+        return None
+    
+    input_image = Image.open(input_data).convert("RGB")
+	
+    # resize image, keep aspect ratio
+    h = input_image.size[0]
+    w = input_image.size[1]
+    ratio = h *1.0 / w
+    if ratio > 1:
+        h = LOAD_SIZE
+        w = int(h*1.0/ratio)
+    else:
+        w = LOAD_SIZE
+        h = int(w * ratio)
+    input_image = input_image.resize((h, w), Image.BICUBIC)
+    input_image = np.asarray(input_image)
 
-
-for files in os.listdir(input_dir):
-	ext = os.path.splitext(files)[1]
-	if ext not in ['.jpg', '.png']:
-		continue
-	
-    # load image
-	input_image = Image.open(os.path.join(input_dir, files)).convert("RGB")
-	
-	# resize image, keep aspect ratio
-	h = input_image.size[0]
-	w = input_image.size[1]
-	ratio = h *1.0 / w
-	if ratio > 1:
-		h = load_size
-		w = int(h*1.0/ratio)
-	else:
-		w = load_size
-		h = int(w * ratio)
-	input_image = input_image.resize((h, w), Image.BICUBIC)
-	input_image = np.asarray(input_image)
-	
-	# RGB -> BGR
-	input_image = input_image[:, :, [2, 1, 0]]
-	input_image = transforms.ToTensor()(input_image).unsqueeze(0)
+    # RGB -> BGR
+    input_image = input_image[:, :, [2, 1, 0]]
+    input_image = transforms.ToTensor()(input_image).unsqueeze(0)
 	
     # preprocess, (-1, 1)
-	input_image = -1 + 2 * input_image 
-	
-	
-    # forward
-	output_image = model(input_image)
-	output_image = output_image[0]
+    input_image = -1 + 2 * input_image 
+    
+    with torch.no_grad():
+        input_data = input_data.to(device)
+        cartoonized = model(input_data)
+
+    output_image = cartoonized[0]
 	# BGR -> RGB
-	output_image = output_image[[2, 1, 0], :, :]
+    output_image = output_image[[2, 1, 0], :, :]
 	# deprocess, (0, 1)
-	output_image = output_image.data.cpu().float() * 0.5 + 0.5
-	
-	# save
-	vutils.save_image(output_image, os.path.join(output_dir, files[:-4] + '_' + style + '.jpg'))
+    output_image = output_image.data.cpu().float() * 0.5 + 0.5
+
+    return output_image
